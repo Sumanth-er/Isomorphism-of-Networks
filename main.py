708250b9415322507d4a8b77c1414fd2b5a4fa8d
Sumanth-er <73772519+Sumanth-er@users.noreply.github.com>
2022-04-08 07:42:47 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,67 +1,69 @@
-# Add a vertex to the set of vertices and the graph
-def add_vertex(v):
-  global graph
-  global vertices_no
-  global vertices
-  if v in vertices:
-    print("Vertex ", v, " already exists")
-  else:
-    vertices_no = vertices_no + 1
-    vertices.append(v)
-    if vertices_no > 1:
-        for vertex in graph:
-            vertex.append(0)
-    temp = []
-    for i in range(vertices_no):
-        temp.append(0)
-    graph.append(temp)
 
-# Add an edge between vertex v1 and v2 with edge weight e
-def add_edge(v1, v2, e):
-    global graph
-    global vertices_no
-    global vertices
-    # Check if vertex v1 is a valid vertex
-    if v1 not in vertices:
-        print("Vertex ", v1, " does not exist.")
-    # Check if vertex v1 is a valid vertex
-    elif v2 not in vertices:
-        print("Vertex ", v2, " does not exist.")
-    # Since this code is not restricted to a directed or
-    # an undirected graph, an edge between v1 v2 does not
-    # imply that an edge exists between v2 and v1
-    else:
-        index1 = vertices.index(v1)
-        index2 = vertices.index(v2)
-        graph[index1][index2] = e
+class Graph(object):
 
-# Print the graph
-def print_graph():
-  global graph
-  global vertices_no
-  for i in range(vertices_no):
-    for j in range(vertices_no):
-      if graph[i][j] != 0:
-        print(vertices[i], " -> ", vertices[j], \
-        " edge weight: ", graph[i][j])
+    def __init__(self, size):
+        self.adjMatrix = []
+        for i in range(size):
+            self.adjMatrix.append([0 for i in range(size)])
+        self.size = size
 
-# Driver code
-# stores the vertices in the graph
-vertices = []
-# stores the number of vertices in the graph
-vertices_no = 0
-graph = []
-# Add vertices to the graph
-add_vertex(1)
-add_vertex(2)
-add_vertex(3)
-add_vertex(4)
-# Add the edges between the vertices by specifying
-# the from and to vertex along with the edge weights.
-add_edge(1, 2, 1)
-add_edge(1, 3, 1)
-add_edge(2, 3, 3)
-add_edge(3, 4, 4)
-add_edge(4, 1, 5)
 
-print(graph)
+    def add_connection(self, v1, v2):
+        if v1 == v2:
+            print("Same vertex %d and %d" % (v1, v2))
+        self.adjMatrix[v1][v2] = 1
+        self.adjMatrix[v2][v1] = 1
+
+    def remove_edge(self, v1, v2):
+        if self.adjMatrix[v1][v2] == 0:
+            print("No edge between %d and %d" % (v1, v2))
+            return
+        self.adjMatrix[v1][v2] = 0
+        self.adjMatrix[v2][v1] = 0
+
+    def __len__(self):
+        return self.size
+
+    def print_matrix(self):
+        for row in self.adjMatrix:
+            for val in row:
+                print('{:4}'.format(val)),
+            print
+    def isomorphic(g,g2):
+        count = 0
+        for i in g:
+            if i == 1:
+                count = count + 1
+        for j in g2:
+            if i == 1:
+                if i == 1:
+                    count = count - 1
+        if count == 0:
+            print("the graph is isomorphic")
+        else:
+            print("the gramph is not isomorphic")
+
+def main():
+    g = Graph(5)
+    g.add_connection(0, 1)
+    g.add_connection(0, 2)
+    g.add_connection(1, 2)
+    g.add_connection(2, 0)
+    g.add_connection(2, 3)
+
+    g.print_matrix()
+
+    g2 = Graph(5)
+    g2.add_connection(0, 1)
+    g2.add_connection(0, 2)
+    g2.add_connection(1, 2)
+    g2.add_connection(2, 0)
+    g2.add_connection(2, 3)
+    print("network 2")
+    g2.print_matrix()
+
+    print(type(g))
+    print(Graph.isomorphic(g,g2))
+
+if __name__ == '__main__':
+    main()
